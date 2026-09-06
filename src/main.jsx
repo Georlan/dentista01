acc59f676fb24734475e261f16fa628bbb1daa0a
Georlan <129700145+Georlan@users.noreply.github.com>
2026-09-06 16:43:26 -0300

    feat: keep hero team carousel continuously rotating

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppV2 from './AppV2';
+import HeroTeamAutoRotation from './HeroTeamAutoRotation';
 import './app-v2.css';
 import './hero-team.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppV2 />
+    <HeroTeamAutoRotation />
   </React.StrictMode>,
 );
