24ca22637c2e5d2ebf476b78512fc6a5628d57ea
Georlan <129700145+Georlan@users.noreply.github.com>
2026-09-07 01:38:32 -0300

    feat: activate premium motion experience

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppV2 from './AppV2';
 import HeroTeamAutoRotation from './HeroTeamAutoRotation';
+import MotionExperience from './MotionExperience';
 import './app-v2.css';
 import './hero-team.css';
+import './motion-v3.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppV2 />
     <HeroTeamAutoRotation />
+    <MotionExperience />
   </React.StrictMode>,
 );
