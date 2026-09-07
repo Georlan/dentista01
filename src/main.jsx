1253911e3e8dbb65eea6adc5d76273e817bb9eec
Georlan <129700145+Georlan@users.noreply.github.com>
2026-09-07 10:29:06 -0300

    feat: activate premium editorial v3

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppV2 from './AppV2';
-import HeroTeamAutoRotation from './HeroTeamAutoRotation';
-import MotionExperience from './MotionExperience';
-import BrandMoments from './BrandMoments';
-import './app-v2.css';
-import './hero-team.css';
-import './motion-v3.css';
-import './brand-v4.css';
+import AppV3 from './AppV3';
+import './app-v3.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppV2 />
-    <HeroTeamAutoRotation />
-    <MotionExperience />
-    <BrandMoments />
+    <AppV3 />
   </React.StrictMode>,
 );
