6e764c18f3a0af1ff4666907b7d2ab9b7e1d2a6e
Georlan <129700145+Georlan@users.noreply.github.com>
2026-09-07 01:43:54 -0300

    feat: activate brand motion v4

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import AppV2 from './AppV2';
 import HeroTeamAutoRotation from './HeroTeamAutoRotation';
 import MotionExperience from './MotionExperience';
+import BrandMoments from './BrandMoments';
 import './app-v2.css';
 import './hero-team.css';
 import './motion-v3.css';
+import './brand-v4.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppV2 />
     <HeroTeamAutoRotation />
     <MotionExperience />
+    <BrandMoments />
   </React.StrictMode>,
 );
